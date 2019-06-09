6d3bc3a717a73e1344d3714983e1abf2f661e117
GHakshay <akshay.at009@gmail.com>
2019-06-09 17:47:45 +0000

    first attempt

---
+++ problem1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from datetime import date
+name=input("enter you name")
+age=int(input("enter your age":))
+
+cy=date.today().year
+a=95-age
+print("year when age ll be 95" cy+a)
