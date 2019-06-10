084818a3c11df7744ac6d391968b65dff2ad9e6c
GHakshay <akshay.at009@gmail.com>
2019-06-10 13:48:16 +0000

    pending

---
+++ web_program.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+#  now importing  time library 
+import  webbrowser 
+import  time
+import  subprocess
+option='''
+Press  1  to  start  your  vlc  media player :
+Press  2  to  play your fav song in youtube :  
+Press  3  to  search something  on google   :  
+Press  4  to send whatsapp message to your fav number  :  
+Press  5  to  check current  time and date  :
+press  6  to reboot your machine  : 
+'''
+print(option)
+
+#  taking  input from  user  
+#  1 st
+choice=input()
+#  input function will take input in str  format 
+#   conditional  state
+if   choice  ==   '5' :
+    #   to connect  our BIOS  time 
+    current_time=time.ctime()
+    print(current_time)
+
+elif  choice  ==  '1'  :
+    #   to connect  os level application we use subprocess 
+    subprocess.getoutput('vlc')
+
+elif  choice  ==  '3' :
+    data=input("type your search :--->  ")
+    webbrowser.open_new_tab('https://www.google.com/search?q='+data)
+elif choice=='2' :
+	song=input("Enter song name:")
+	
+	webbrowser.open_new_tab("https://www.youtube.com/results?search_query="+song)
+elif choice=='4':
+	webbrowser.open_new_tab("https://web.whatsapp.com/")
+	#scan the QR code 
+	
+	
