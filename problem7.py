a0300efacc3bf2c0095fa8053c4eecdf42271df3
Akshay Thakur <39756828+GHakshay@users.noreply.github.com>
2019-06-17 17:10:08 +0530

    multiple file creation + access time

--- problem7.py
+++ problem7.py
@@ -1,4 +1,31 @@
-file=input("Enter file name you want to create")
+#!/usr/bin/python3
+
+
+
+
+'''file=input("Enter file name you want to create")
 f=open(file,"a+")
 f.seek(0)
-f.close()
+f.close()'''
+import datetime
+
+atime={}
+curr_time=[]
+no_files=int(input("how many files you want to create: " ))
+for i in range(no_files):
+      file=input("Enter file name you want to create: ")
+      f=open(file,"a+")
+      curr_time.append(datetime.datetime.now())
+
+      atime[file]=curr_time[i]
+
+
+print(atime)
+
+answer=input("Do you want to see access time ?yes/no ")
+if answer=='yes':
+        f_name=input("enter file name: ")
+        print(atime[f_name])
+else:
+  print("ok")
+    
